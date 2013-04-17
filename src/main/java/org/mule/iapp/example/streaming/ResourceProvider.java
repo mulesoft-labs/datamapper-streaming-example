32cce6e447eda5345c0528e4e01fa7f55f13748a
mariano.simone <mariano.simone@mulesoft.com>
2013-04-17 16:49:32 -0300

    wrapping up example

---
+++ src/main/java/org/mule/iapp/example/streaming/ResourceProvider.java
@@ -0,0 +1,21 @@
+package org.mule.iapp.example.streaming;
+
+import java.io.InputStream;
+
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.processor.MessageProcessor;
+
+public class ResourceProvider implements MessageProcessor {
+
+    /**
+     * Return the same MuleEvent, changing the Payload to a InputStream for the target file
+     */
+    @Override
+    public MuleEvent process(final MuleEvent event) throws MuleException {
+        final InputStream in = this.getClass().getResourceAsStream("/characters.csv");
+        event.getMessage().setPayload(in);
+        return event;
+    }
+
+}
